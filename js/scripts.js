d3fce42305f1f188111fb96e840ebb5c2864e715
Dylan Dills and Isabella Abatgis <isabella@>
2017-04-26 10:01:59 -0700

    add what was needed to perform function in JS, completed html page, and completed css page

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    var noun1Input = $("input#noun1").val();
+    var verb1Input = $("input#verb1").val();
+    var animal1Input = $("input#animal1").val();
+    var noun2Input = $("input#noun2").val();
+    var verb2Input = $("input#verb2").val();
+    var adjectiveInput = $("input#adjective").val();
+
+    $(".noun1").text(noun1Input);
+    $(".verb1").text(verb1Input);
+    $(".animal1").text(animal1Input);
+    $(".noun2").text(noun2Input);
+    $(".verb2").text(verb2Input);
+    $(".adjective").text(adjectiveInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
